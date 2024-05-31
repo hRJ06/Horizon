d7e36e944373a50dd9626dba9618275dfc42c476
hRJ06 <hindolroy.2306@gmail.com>
2024-06-01 00:15:44 +0530

    Added Right Sidebar initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Hindol" };
+  const loggedIn = {
+    firstName: "Hindol",
+    lastName: "Roy",
+    email: "hindolroy.2306@gmail.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
@@ -21,6 +26,7 @@ const Home = () => {
           />
         </header>
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };
