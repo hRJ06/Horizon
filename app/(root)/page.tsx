11ab6b396e4efe77a4b44e1831bb2d7374b430bb
hRJ06 <hindolroy.2306@gmail.com>
2024-07-02 22:46:41 +0530

    Added Server Actions for Bank & Transactions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import RecentTransactions from "@/components/RecentTransactions";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import { getAccounts } from "@/lib/actions/bank.actions";
@@ -6,13 +7,14 @@ import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
   const accounts = await getAccounts({
     userId: loggedIn.$id,
   });
   if (!accounts) return;
   const accountsData = accounts?.data;
-  console.log(accounts)
+  console.log(accounts);
   const appwriteItemId = (id as string) || accountsData[0].appwriteItemId;
   return (
     <section className="home">
@@ -30,6 +32,12 @@ const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
             totalCurrentBalance={accounts?.totalCurrentBalance}
           />
         </header>
+        <RecentTransactions
+          accounts={accountsData}
+          transactions={account?.transactions}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        />
       </div>
       <RightSidebar
         user={loggedIn}
--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -3,7 +3,8 @@
 import { CountryCode } from "plaid";
 import { plaidClient } from "../plaid";
 import { parseStringify } from "../utils";
-import { getBanks } from "./user.actions";
+import { getBank, getBanks } from "./user.actions";
+import { getTransactionsByBankId } from "./transaction.actions";
 
 export const getAccounts = async ({ userId }: getAccountsProps) => {
   try {
@@ -14,7 +15,7 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
           access_token: bank.accessToken,
         });
         const accountData = accountsResponse.data.accounts[0];
-        const institution = await getInstituion({
+        const institution = await getInstitution({
           institutionId: accountsResponse.data.item.institution_id!,
         });
         const account = {
@@ -44,7 +45,65 @@ export const getAccounts = async ({ userId }: getAccountsProps) => {
   }
 };
 
-export const getInstituion = async ({ institutionId }: getInstitutionProps) => {
+export const getAccount = async ({ appwriteItemId }: getAccountProps) => {
+  try {
+    const bank = await getBank({ documentId: appwriteItemId });
+    const accountsResponse = await plaidClient.accountsGet({
+      access_token: bank.accessToken,
+    });
+    // TODO:
+    const accountData = accountsResponse.data.accounts[0];
+    const transferTransactionsData = await getTransactionsByBankId({
+      bankId: bank.$id,
+    });
+    const transferTransactions = transferTransactionsData.documents.map(
+      (transferData: Transaction) => ({
+        id: transferData.$id,
+        name: transferData.name!,
+        amount: transferData.amount!,
+        date: transferData.$createdAt,
+        paymentChannel: transferData.channel,
+        category: transferData.category,
+        type: transferData.senderBankId === bank.$id ? "debit" : "credit",
+      })
+    );
+    const institution = await getInstitution({
+      institutionId: accountsResponse.data.item.institution_id!,
+    });
+
+    const transactions = await getTransactions({
+      accessToken: bank?.accessToken,
+    });
+
+    const account = {
+      id: accountData.account_id,
+      availableBalance: accountData.balances.available!,
+      currentBalance: accountData.balances.current!,
+      institutionId: institution.institution_id,
+      name: accountData.name,
+      officialName: accountData.official_name,
+      mask: accountData.mask!,
+      type: accountData.type as string,
+      subtype: accountData.subtype! as string,
+      appwriteItemId: bank.$id,
+    };
+
+    const allTransactions = [...transactions, ...transferTransactions].sort(
+      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+    );
+
+    return parseStringify({
+      data: account,
+      transactions: allTransactions,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getInstitution = async ({
+  institutionId,
+}: getInstitutionProps) => {
   try {
     /* https://plaid.com/docs/api/institutions/#institutionsget*/
     const institutionResponse = await plaidClient.institutionsGetById({
@@ -53,6 +112,37 @@ export const getInstituion = async ({ institutionId }: getInstitutionProps) => {
     });
     const institution = institutionResponse.data.institution;
     return parseStringify(institution);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTransactions = async ({
+  accessToken,
+}: getTransactionsProps) => {
+  /* https://plaid.com/docs/api/products/transactions/#transactionssync */
+  let hasMore = true;
+  let transactions: any = [];
+  try {
+    while (hasMore) {
+      const response = await plaidClient.transactionsSync({
+        access_token: accessToken,
+      });
+      const data = response.data;
+      transactions = response.data.added.map((transaction) => ({
+        id: transaction.transaction_id,
+        name: transaction.name,
+        paymentChannel: transaction.payment_channel,
+        accountId: transaction.account_id,
+        amount: transaction.amount,
+        pending: transaction.pending,
+        category: transaction.category ? transaction.category[0] : "",
+        date: transaction.date,
+        image: transaction.logo_url,
+      }));
+      hasMore = data.has_more;
+    }
+    return parseStringify(transactions);
   } catch (error) {
     console.error(error);
   }
